55ec2a0c8760c5009c4ada886f66c00107575c54
Thiago Dias <tdtadeu@gmail.com>
2022-04-02 14:53:04 -0300

    Add boundary

--- mix.exs
+++ mix.exs
@@ -24,7 +24,9 @@ defmodule BoundaryTest.Umbrella.MixProject do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps/ folder.
   defp deps do
-    []
+    [
+      {:boundary, "~> 0.9", runtime: false}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
