7c87e7b4775172ceeb5a4f55012e802b9c5dc2ab
Thiago Dias <tdtadeu@gmail.com>
2022-04-02 16:19:37 -0300

    Make project compililation raise forbidden warnings

--- apps/boundary_test_web/lib/boundary_test_web/controllers/user_controller.ex
+++ apps/boundary_test_web/lib/boundary_test_web/controllers/user_controller.ex
@@ -3,6 +3,8 @@ defmodule BoundaryTestWeb.UserController do
 
   def index(conn, _params) do
     BoundaryTest.foo()
+    BoundaryTest.Accounts.foo()
+    BoundaryTest.Accounts.User.foo()
     send_resp(conn, 200, "ok")
   end
 end
